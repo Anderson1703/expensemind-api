7b2fe96211369db76079e1718658bfeaf3433064
Anderson Encarnacion <adamesgithub@gmail.com>
2025-05-25 16:43:29 -0400

    t

--- src/schemas/expenses.schema.ts
+++ src/schemas/expenses.schema.ts
@@ -1,21 +1,23 @@
-
 import { z } from "zod";
 
 export const ExpenseCreateSchema = z.object({
-    categoryId: z.string(),
-    date: z.string().transform((date) => new Date(date)),
-    totalAmount: z.number(),
-    urlFile: z.string().optional(),
-    info: z.string().optional(),
-    business: z.string().optional()
+  categoryId: z.string(),
+  date: z.string().transform((date) => new Date(date)),
+  totalAmount: z.number(),
+  urlFile: z.string().optional(),
+  info: z.string().optional(),
+  business: z.string().optional(),
 });
 
 export const ExpenseUpdateSchema = z.object({
-    date: z.date().optional(),
-    totalAmount: z.number().optional(),
-    urlFile: z.string().optional(),
-    info: z.string().optional(),
-    business: z.string().optional()
+  date: z
+    .string()
+    .transform((date) => new Date(date))
+    .optional(),
+  totalAmount: z.number().optional(),
+  urlFile: z.string().optional(),
+  info: z.string().optional(),
+  business: z.string().optional(),
 });
 
 export type ExpenseCreateType = z.infer<typeof ExpenseCreateSchema>;
